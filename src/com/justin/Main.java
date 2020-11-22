8462c1554814c6cfeda3bc3abd819f639a76f62e
Justinnn07 <justinv2006@gmail.com>
2020-11-22 14:43:38 +0530

    initial commit

--- src/com/justin/Main.java
+++ src/com/justin/Main.java
@@ -1,10 +1,19 @@
 package com.justin;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args){
         int age = 15; // for numbers
         System.out.println(age); // variable
         System.out.println("Hello world"); // string
+        Scanner sc = new  Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("What is your no.");
+        int b = sc.nextInt();
+        System.out.print("Whats the second no.");
+        int c = a + b;
+        System.out.println(c);
     }
 }
